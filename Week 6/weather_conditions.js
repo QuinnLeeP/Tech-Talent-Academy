83e204084aecff773821182d468a15a0f31052ed
Quinn Lee Phillips <quinnphillips35@hotmail.com>
2022-04-08 21:37:07 +0100

    Week 6 HLT

---
+++ Week 6/weather_conditions.js
@@ -0,0 +1,10 @@
+let temperature = 42;
+
+let condition = "Sunny" ;
+
+let goToWork = "";
+
+if (temperature >= 10 && condition === "Sunny") {
+    goToWork = true;
+    console.log("You can go to work and continue construction on your lab");
+} 
