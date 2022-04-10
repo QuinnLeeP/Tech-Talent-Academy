e2e8210477344e19fb38b3260350d6e4d7656d79
Quinn Lee Phillips <quinnphillips35@hotmail.com>
2022-04-10 13:30:36 +0100

    JS-Weather

--- Week 6/weather_conditions.js
+++ Week 6/weather_conditions.js
@@ -1,10 +1,54 @@
-let temperature = 42;
+let temperature = 24;
 
-let condition = "Sunny" ;
+let condition = "Sunny";
 
 let goToWork = "";
 
 if (temperature >= 10 && condition === "Sunny") {
     goToWork = true;
-    console.log("You can go to work and continue construction on your lab");
-} 
+} else if (temperature >= 10 && condition === "Cloudy") {
+  goToWork == true;
+} else if (temperature >= 10 && condition === "Raining") {
+  goToWork == false;
+} else if (temperature >= 10 && condition === "Snowing"){
+  goToWork == false;
+} else if (temperature >= 10 && condition === "Thunder") {
+  goToWork == false;
+} else if (temperature > 20 && condition === "Sunny"){
+  goToWork == true;
+} else if (temperature > 20 && condition === "Cloudy") {
+  goToWork == true;
+} else if (temperature > 20 && condition === "Raining") {
+  goToWork == true;
+} else if (temperature > 20 && condition === "Snowing"){
+  goToWork == false;
+} else if (temperature > 20 && condition === "Thunder") {
+  goToWork == false;
+} else if (temperature > 30 && condition === "Sunny"){
+  goToWork == false;
+} else if (temperature > 30 && condition === "Cloudy") {
+  goToWork == false;
+} else if (temperature > 30 && condition === "Raining") {
+  goToWork == false;
+} else if (temperature > 30 && condition === "Snowing"){
+  goToWork == false;
+} else if (temperature > 30 && condition === "Thunder") {
+  goToWork == false;
+} else if (temperature < 10 && condition === "Sunny"){
+  goToWork == false;
+} else if (temperature < 10 && condition === "Cloudy") {
+  goToWork == false;
+} else if (temperature < 10 && condition === "Raining") {
+  goToWork == false;
+} else if (temperature < 10 && condition === "Snowing"){
+  goToWork == false;
+} else if (temperature < 10 && condition === "Thunder") {
+  goToWork == false;
+}
+
+
+if (goToWork == true) {
+  console.log("You can go to work and continue construction on your lab")
+} else {
+  console.log("The weather's not right, don't go to work today")
+}
